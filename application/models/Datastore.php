0b57e84a2cf6b61a872983a02909861dcc28adbe
Robert Scherf <T923029@corp.ads>
2017-05-08 13:42:23 -0600

    server returns query results as array of arrays

--- application/models/Datastore.php
+++ application/models/Datastore.php
@@ -283,26 +283,58 @@ class Datastore extends CI_Model {
         return $return_row;
     }
     
+    private function make_header_row($incoming, $result_array) {
+        // Given client query and array of results, construct the query's header row.
+        // First, add the row field(s) specified by the user.
+        // Next, add the null column value (if it exists).
+        // Finally, sort remaining column entries in ascending order and add them to the header array.
+        // This array is the first entry in the JSON result array. All result rows present data in the order of this array.
+        $query_cols = [];
+        foreach ($incoming['model']['Rows'] as $row_obj) {
+            $query_cols[] = $row_obj['name'];
+        }
+        if (array_key_exists("''", $result_array)) {
+            $query_cols[] = '';
+        }
+        ksort($result_array[0]);
+        foreach($result_array[0] as $key=>$val) {
+            if (!in_array($key, $query_cols)) {
+                $query_cols[] = $key;
+            }
+        }
+        return $query_cols;
+    }
+    
+    private function flatten_result_array($header, $result_array) {
+        // Given the table header (an array containing column titles in correct order)
+        // and an array of sql results, put all results into a flat array matching
+        // the order of the header.
+        
+        $flat_results = [$header];
+        foreach($result_array as $row) {
+            $flat_row = [];
+            foreach($header as $title) {
+                $flat_row[] = $row[$title];
+            }
+            $flat_results[] = $flat_row;
+        }
+        return $flat_results;
+    }
+    
+    
     public function process_query($incoming) {
-        set_time_limit(300);
+        //set_time_limit(300);
         $result = $this->Queryparser->make_pivot_query($incoming);
         if ($result == false) {
             return $result;
         }
         
         $query = $this->db->query($result);
+        $query_result = $query->result_array();
         
-        // SQL QUERY SENT TO ORACLE
-        log_message('debug', $result);
-        
-        // REMOVE SINGLE QUOTES FROM ROW ARRAY KEYS
-        $return_result = array_map(array($this, 'unwrap_keys'), $query->result_array());
-        
-        // foreach($return_result as $row) {
-        //     log_message('debug', json_encode($row));
-        // }
-        
-        return $return_result;
+        $header = $this->make_header_row($incoming, $query_result);
+        $flat_results = $this->flatten_result_array($header, $query_result);
+        return $flat_results;
     }
     
 }
