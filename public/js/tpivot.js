aac50e65d50f4de1c0a2fb980207cb9b78445d95
Robert Scherf <rgscherf@gmail.com>
2017-06-27 10:24:55 -0600

    implement droppable table

--- public/js/tpivot.js
+++ public/js/tpivot.js
@@ -1,24 +1,174 @@
 var tpivot = (function () {
+
+    //////////////
+    // UTILITY FNS
+    //////////////
+
+    var tableRows = function () {
+        // Return all tr elements of the sortable table.
+        return $(".sortableTable__body > tr");
+    }
+
+    var title = function (str) {
+        // Capitalize first letter of a string.
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
+
+    ////////////////
+    // COLUMN HIDING
+    ////////////////
+
+    var dehydrateColumn = function (event) {
+        // Store column data in a button and remove column from the sortable table.
+        var tar = $(event.target);
+        var colData = tar.data("col");
+
+        if (!colData) { return; }
+
+        var matchingCells = orderedValuesOfCol(colData);
+        var store = {
+            header: colData,
+            values: matchingCells.map(function (elem) {
+                return elem.clone(true);
+            })
+        };
+        var btn = $("<button>")
+            .text(title(colData))
+            .data("column", store)
+            .appendTo($(".sortableTable__discard"))
+            .click(function (event) {
+                hydrateColumn(event);
+            });
+
+        tar.remove();
+        $(matchingCells).each(function (idx, elem) {
+            elem.remove();
+        });
+    }
+
+    var hydrateColumn = function (event) {
+        var dat = $(event.target).data();
+        var values = dat.column.values;
+        var head = dat.column.header;
+        var th = $("<th>")
+            .text(title(head))
+            .attr("data-col", head)
+            .addClass("value")
+            .addClass("ui-sortable-handle")
+            .dblclick(function (event) {
+                dehydrateColumn(event);
+            });
+        $(".sortableTable__header").append(th);
+        event.target.remove();
+
+        tableRows().each(function (idx) {
+            $(this).append(values[idx]);
+        });
+    }
+
+    var orderedValuesOfCol = function (colName) {
+        // Return all td elements matching a given th element.
+        var ret = [];
+        tableRows().each(function (idx, row) {
+            var cellValue = getMatchingCell($(this), colName);
+            ret.push(cellValue);
+        });
+        return ret;
+    }
+
+
+    /////////////////
+    // COLUMN SORTING
+    /////////////////
+
+    var sortcells = function (item) {
+        // Move tds matching associated sorted th to the same index as the th.
+        var newIndex = $(".sortableTable__header").children().index(item);
+        var column = item.data("col");
+
+        tableRows().each(function (idx, row) {
+            var matchingDataCol = getMatchingCell($(this), column);
+            moveTo($(this), newIndex, $(matchingDataCol));
+        });
+    }
+
+    var getMatchingCell = function (container, columnData) {
+        // Retrieve elment from a collection matching certain data attribute.
+        var ret = container
+            .children()
+            .filter(function () {
+                return $(this).data("col") === columnData;
+            })
+            .first();
+        return ret;
+    }
+
+    var moveTo = function (container, index, element) {
+        // Move an element to a certin index within a container.
+        // Element is first removed from children(), then then inserted.
+        // The length of children() may change in between.
+        var movingLeft = index < element.index();
+        var elementAtGivenIndex = container.children().eq(index);
+        if (movingLeft) {
+            elementAtGivenIndex.before(element);
+        } else {
+            elementAtGivenIndex.after(element);
+        }
+    }
+
+
+    ///////////
+    // CREATION
+    ///////////
+
     var makeTableHeaderRow = function (tableComponent, headings) {
         // Make a row of `th` cells and append to a table component (most likely a `thead`).
-        var tr = $('<tr></tr>');
+        var tr = $('<tr></tr>')
+            .addClass('sortableTable__header')
+            .sortable({
+                placeholder: "placeholder",
+                items: "> .value",
+                helper: "clone",
+                revert: 150,
+                axis: "x",
+                change: function (event, ui) {
+                    console.log(ui.placeholder);
+                    ui.placeholder.width(ui.helper.width);
+                },
+                start: function (event, ui) {
+                    ui.placeholder.width(ui.item.width());
+                },
+                stop: function (event, ui) {
+                    sortcells(ui.item);
+                }
+            })
+            .disableSelection();
         headings.forEach(function (elem) {
-            var th = $('<th></th>');
             // A blank column header ('') is a NULL value.
             var data = elem == "''" ? "NULL" : elem;
+            var th = $('<th></th>')
+                .addClass('value')
+                .attr('data-col', data)
+                .dblclick(function (event) {
+                    dehydrateColumn(event);
+                });
             th.html(data);
             th.appendTo(tr);
         });
         tr.appendTo(tableComponent);
     };
 
-    var makeSingleTableRow = function (tableComponent, rowData) {
+    var makeSingleTableRow = function (tableComponent, rowData, headerRow) {
         // Make a single table body row and append it to a table component (most likely a `tbody`).
         var row = $('<tr></tr>');
         rowData.forEach(function (val, idx) {
             // Blank row headers ('') should display as NULL.
+            var headerData = headerRow[idx] == "''" ? 'NULL' : headerRow[idx];
             var data = idx === 0 ? val || "NULL" : val || "";
-            var cell = $('<td></td>').addClass("pvtVal");
+            var cell = $('<td></td>')
+                .addClass("pvtVal")
+                .attr('data-col', headerData);
             cell.html(data);
             cell.appendTo(row);
         });
@@ -30,16 +180,18 @@ var tpivot = (function () {
             .addClass("pvtTable");
         table.appendTo(container);
 
-        var thead = $('<thead></thead>');
+        var thead = $('<thead></thead>')
+            .addClass('sortableTable');
         makeTableHeaderRow(thead, pivotData[0]);
         thead.appendTo(table);
 
-        var tbody = $('<tbody></tbody>');
+        var tbody = $('<tbody></tbody>')
+            .addClass('sortableTable__body');
         pivotData.forEach(function (row, idx) {
             if (idx === 0) {
                 return;
             }
-            makeSingleTableRow(tbody, row);
+            makeSingleTableRow(tbody, row, pivotData[0]);
         });
         tbody.appendTo(table);
     };
@@ -67,9 +219,11 @@ var tpivot = (function () {
         var headerDiv = $('<div></div>')
             .addClass('queryBuilder--headerText')
             .text('Query results');
+        var discard = $('<div>')
+            .addClass('sortableTable__discard');
 
         containerDiv.appendTo($('#pivotTarget'));
-        containerDiv.append(spacerDiv, headerDiv);
+        containerDiv.append(spacerDiv, headerDiv, discard);
         return containerDiv;
     };
 
