48db4d13b9b4dd582d751fd97496236eb9125f51
Robert Scherf <rgscherf@gmail.com>
2017-09-25 09:30:28 -0700

    pull down external css/js deps

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
-
 <head>
   <title>T Pivot</title>
-  <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
-  <script src="https://use.fontawesome.com/fc36ac97a1.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.min.js"></script>
+
+  <script src="<?php echo base_url('public/js/external/jquery-3.2.1.min.js') ?>"></script>
+  <script src="<?php echo base_url('public/js/external/jquery-ui.min.js') ?>"></script>
+  <script src="<?php echo base_url('public/js/external/bootstrap.min.js') ?>"></script>
+  <script src="<?php echo base_url('public/js/external/Chart.min.js') ?>"></script>
+
   <script src="<?php echo base_url('public/js/tutils.js') ?>"></script>
   <script src="<?php echo base_url('public/js/contextMenus.js') ?>"></script>
   <script src="<?php echo base_url('public/js/selectionPaneUtils.js') ?>"></script>
@@ -17,7 +18,11 @@
   <script src="<?php echo base_url('public/js/queryStorage.js') ?>"></script>
   <script src="<?php echo base_url('public/js/tchart.js') ?>"></script>
   <script src="<?php echo base_url('public/js/tpivot.js') ?>"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('public/css/external/bootstrap-theme.min.css') ?>" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('public/css/external/bootstrap.min.css') ?>" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url('public/css/external/font-awesome.min.css') ?>" />
+
   <link rel="stylesheet" type="text/css" href="<?php echo base_url('public/css/style.css') ?>" />
   <link rel="stylesheet" type="text/css" href="<?php echo base_url('public/css/pivot.css') ?>" />
 </head>
