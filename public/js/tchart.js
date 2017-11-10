077889c19ef5ba97d0e6597f8fac1e3846f941ee
Robert Scherf <rgscherf@gmail.com>
2017-11-10 12:48:26 -0800

    add charting improvements

--- public/js/tchart.js
+++ public/js/tchart.js
@@ -3,17 +3,32 @@ var tchart = (function () {
         $('#pivotChart').remove();
     }
 
+    function getChartTitle(result, model) {
+        // "(STATUS, DUE_DATE) by (TYPE), collected by (COUNT(ID))"
+        function makeEntryString(elems) {
+            var elemString = elems.map(function (elem) {
+                return elem.name;
+            }).join(', ');
+            return elemString;
+        }
+        var rowString = model.Rows.length === 0 ? '*' : makeEntryString(model.Rows);
+        var colString = model.Columns.length === 0 ? '*' : makeEntryString(model.Columns);
+        var aggString = result.meta.aggregators.length === 0
+            ? 'COUNT(*)'
+            : makeEntryString(result.meta.aggregators.map(function (e) { return { name: e }; }));
+        return 'Rows: ' + rowString + '; Columns: ' + colString + '; Collected by: ' + aggString;
+    }
+
     function renderExpChart(model, result, chartType) {
         removeChart();
         var metaCoords = tutils.allMetaCoordinates(result);
-        var xAxisLabels = metaCoords.colCoords.map(function (elem) {
-            return elem.join('/');
-        });
+        var rowCoords = result.meta.specificRowOrder ? result.meta.specificRowOrder : metaCoords.rowCoords; // accommodating sort-by-value
+        var xAxisLabels = [];
 
         var datums = [];
         // we want each row*agg to be a datum.
         result.meta.aggregators.forEach(function (aggName, aggIdx) {
-            metaCoords.rowCoords.forEach(function (rowCoord) {
+            rowCoords.forEach(function (rowCoord) {
                 var rowData = metaCoords.colCoords.map(function (colCoord) {
                     if (result.results[rowCoord]
                         && result.results[rowCoord][colCoord]
@@ -24,9 +39,11 @@ var tchart = (function () {
                         return 0;
                     }
                 });
-                var rowLabel = model['Values'][aggIdx].reducer + '(' + model['Values'][aggIdx].name + ') of ' + rowCoord.join('/');
+                var tooltipLabel = rowCoord.join('/');
+                var thisXAxisLabel = [aggName, tooltipLabel];
+                xAxisLabels.push(thisXAxisLabel);
                 var thisDatum = {
-                    label: rowLabel,
+                    label: tooltipLabel,
                     data: rowData,
                     backgroundColor: randomColor(),
                     fill: false,
@@ -36,14 +53,18 @@ var tchart = (function () {
                 datums.push(thisDatum);
             });
         });
-        var yAxes = model['Values'].map(function (elem, idx) {
+
+        var yAxes = result.meta.aggregators.map(function (elem, idx) {
             return {
                 id: idx.toString(),
                 type: 'linear',
                 position: 'left',
                 scaleLabel: {
                     display: true,
-                    labelString: elem.reducer + '(' + elem.name + ')'
+                    labelString: elem
+                },
+                ticks: {
+                    beginAtZero: true
                 }
             };
         });
@@ -66,7 +87,7 @@ var tchart = (function () {
             height: 'auto',
             width: 820,
             modal: true,
-            title: tutils.describeModel(model),
+            title: getChartTitle(result, model),
             closeOnEscape: true
         })
             .css('padding', '10px');
@@ -74,26 +95,34 @@ var tchart = (function () {
         var chart = new Chart(canvas, {
             type: chartType,
             data: {
-                labels: xAxisLabels,
-                datasets: datums
+                datasets: datums,
             },
             options: {
-                responsive: false,
                 tooltips: {
-                    mode: 'point'
+                    mode: 'point',
+                    callbacks: {
+                        title: function () { }
+                    }
                 },
                 legend: {
                     display: false,
-                    labels: {
-                        display: false
-                    }
                 },
                 scales: {
                     yAxes: yAxes,
                     xAxes: [{
+                        categoryPercentage: 1.0,
+                        barPercentage: 0.8,
+                        offset: true,
+                        type: 'category',
+                        labels: xAxisLabels,
                         id: 'x',
+                        gridLines: {
+                            display: false,
+                            offsetGridLines: true
+                        },
                         ticks: {
-                            autoSkip: false
+                            autoSkip: false,
+                            maxRotation: 90
                         }
                     }]
                 }
