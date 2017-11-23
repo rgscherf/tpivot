84061bc32fabfe9f6cffef72cb5f07b5f86ec371
Robert Scherf <rgscherf@gmail.com>
2017-11-23 11:16:30 -0800

    cleanups for querystorage

--- public/js/queryStorage.js
+++ public/js/queryStorage.js
@@ -11,6 +11,7 @@ var queryStore = (function () {
     }
 
     function addLoadedDataTag(loadData) {
+        // Add DOM element that says "you are currently editing query X"
         currentLoaded = loadData;
         $('#loadedDocument').children().remove();
         var loadString = "Currently modifying query " + loadData.name;
@@ -27,6 +28,8 @@ var queryStore = (function () {
     }
 
     function sendQueryToStorage(tableInfo, model, name, eventType, id) {
+        // Given a query and its metadata, send the query to be stored on server.
+        // 'new' queries won't have an ID, so we'll send an undefined value. server handles this gracefully.
         var omnibusModel = {
             db: tableInfo.db,
             owner: tableInfo.owner,
@@ -81,6 +84,7 @@ var queryStore = (function () {
     }
 
     function createLabelNameWidget(containingElement, currentTableInfo, model) {
+        // Create widget for saving a table name. 
         function saveQueryWithName() {
             var queryName = $('#queryNameInput').val();
             $('.queryNameContainer').remove();
@@ -108,15 +112,21 @@ var queryStore = (function () {
                     saveQueryWithName();
                 }
             })
+            .keyup(function (event) {
+                var key = event.which;
+                if (key === 27) { // esc
+                    $('.queryNameContainer').remove();
+                }
+            })
             .click(function (event) {
                 event.stopPropagation();
                 $('#queryNameInput').focus();
             })
-            .css({ 'width': '150px', 'margin-right': '10px', 'height': '34px', 'padding': '5px' })
+            .css({ 'width': '200px', 'margin-right': '10px', 'height': '34px', 'padding': '5px' })
             .attr({
                 'id': 'queryNameInput',
                 'type': 'text',
-                'placeholder': 'Name this query:'
+                'placeholder': 'Name this query. ESC cancels.'
             });
         var ok = $('<button>')
             .addClass('btn btn-warning')
@@ -135,6 +145,7 @@ var queryStore = (function () {
     }
 
     function whenTableIsLoaded(loadData) {
+        // To be run when we're ready to load table data.
         return function () {
             data.model = loadData.model;
             var buckets = ["Values", "Filters", "Rows", "Columns"];
@@ -150,6 +161,7 @@ var queryStore = (function () {
     }
 
     function loadQueryFromModel(loadData) {
+        // After a saved query is selected for loading, orchestrate DOM and model changes to retrieve and display data.
         $('#getTable').removeClass('btn-warning').addClass('btn-default');
         $('#dbSelector').val(loadData.db);
         var tableIdentifier = loadData.type + ': ' + loadData.owner + "." + loadData.table;
@@ -162,38 +174,10 @@ var queryStore = (function () {
         view.switchToNewTable(tableIdentifier, runWhenTableLoaded);
     }
 
-    function menuFromQueries(queries) {
-        var container = $('<div class="loadMenu">');
-        var d = $('<table>');
-        var t = $('<tr class="queryBuilder--headerText">')
-            .append($('<th>').text('ID'))
-            .append($('<th>').text('Name'))
-            .append($('<th>').text('Table Location'))
-            // .append($('<th>').text('Query Description'))
-            // .append($('<th>').text('Transformations'))
-            .append($('<th>').text('Date Saved'))
-            .appendTo(d);
-        queries.forEach(function (elem) {
-            var q = $('<tr>')
-                .click(function (event) {
-                    loadQueryFromModel(elem);
-                    $('.loadQueryMenu').dialog("close");
-                })
-                .append($('<td>').text(elem.id))
-                .append($('<td>').text(elem.name))
-                .append($('<td>').text(elem.db + "." + elem.owner + "." + elem.table + '(' + elem.type + ')'))
-                // .append($('<td>').text(tutils.describeModel(elem.model)))
-                // .append($('<td>').text(tutils.describeTransform(elem.transform)))
-                .append($('<td>').text(elem.date))
-                .disableSelection();
-            q.appendTo(d);
-        });
-        container
-            .append(d);
-        return container;
-    }
-
     function unpackReturnedQueries(queries) {
+        // Unpack saved queries returned from the server.
+        // The primary function is to lift tableData values out of the server's 'query' object
+        // Into top-level fields on the client-side object.
         return queries.map(function (parsed) {
             return {
                 db: parsed.omnibusModel.db,
@@ -210,35 +194,76 @@ var queryStore = (function () {
         });
     }
 
+    function loadQueriesIntoMenu(queries) {
+        // Load saved queries (returned from the server) into the query menu.
+        // Remove the spinner first.
+        var d = $('.loadMenuTarget');
+        d.children().remove();
+        d.css('width', '100%');
+        var t = $('<tr class="queryBuilder--headerText">')
+            .append($('<th>').text('ID'))
+            .append($('<th>').text('Name'))
+            .append($('<th>').text('Table Location'))
+            .append($('<th>').text('Date Saved'))
+            .appendTo(d);
+        queries.forEach(function (elem) {
+            var q = $('<tr>')
+                .click(function (event) {
+                    loadQueryFromModel(elem);
+                    $('.loadQueryMenu').dialog("close");
+                })
+                .append($('<td>').text(elem.id))
+                .append($('<td>').text(elem.name))
+                .append($('<td>').text(elem.db + "." + elem.owner + "." + elem.table + '(' + elem.type + ')'))
+                .append($('<td>').text(elem.date))
+                .disableSelection();
+            q.appendTo(d);
+        });
+    }
+
+    function menuFromQueries() {
+        // Initialize the query menu with a spinner.
+        var container = $('<div class="loadMenu">').css('width', '100%');
+        var d = $('<table class="loadMenuTarget">')
+            .append('<i class="fa fa-4x fa-refresh fa-spin" aria-hidden="true"></i>');
+        container
+            .append(d);
+        return container;
+    }
+
     function loadQueryMenu() {
+        // Load the query menu from button click.
+        // Show the menu and a spinner until results return from server.
+        var menuElement = menuFromQueries();
+        $(menuElement)
+            .dialog({
+                classes: {
+                    "ui-dialog": "loadMenu__ui-dialog",
+                    "ui-dialog-titlebar": "loadMenu__ui-dialog-titlebar",
+                    "ui-dialog-title": "loadMenu__ui-dialog-title",
+                    "ui-dialog-titlebar-close": "loadMenu__ui-dialog-close",
+                    // "ui-dialog-content": "",
+                },
+                close: function (event, ui) {
+                    $(this).dialog("destroy");
+                },
+                resizable: false,
+                closeText: "",
+                height: 500,
+                width: 800,
+                modal: true,
+                title: 'Click to load a saved query',
+                closeOnEscape: true
+            })
+            .addClass("loadQueryMenu");
+
         $.ajax({
             type: "get",
             url: window.getSavedQueriesURL,
             data: '',
             success: function (queries) {
                 var unpacked = unpackReturnedQueries(queries);
-                var menuElement = menuFromQueries(unpacked);
-                $(menuElement)
-                    .dialog({
-                        classes: {
-                            "ui-dialog": "loadMenu__ui-dialog",
-                            "ui-dialog-titlebar": "loadMenu__ui-dialog-titlebar",
-                            "ui-dialog-title": "loadMenu__ui-dialog-title",
-                            "ui-dialog-titlebar-close": "loadMenu__ui-dialog-close",
-                            // "ui-dialog-content": "",
-                        },
-                        close: function (event, ui) {
-                            $(this).dialog("destroy");
-                        },
-                        resizable: false,
-                        closeText: "",
-                        height: 500,
-                        width: 800,
-                        modal: true,
-                        title: 'Click to load a saved query',
-                        closeOnEscape: true
-                    })
-                    .addClass("loadQueryMenu");
+                loadQueriesIntoMenu(unpacked);
             },
             error: function (x, stat, err) {
                 console.log("AJAX REQUEST FAILED");
